4a2bf51ee930d30e031ce9e1953843f7b890f775
neeraj kumar <patelneerajkumar@gmail.com>
2014-01-22 10:40:15 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -1,5 +1,4 @@
 
-
 '''this is my program'''
 def  myfunction():
 	n=60085
@@ -19,3 +18,23 @@ def  myfunction():
 
 if __name__=='__main__':
 	myfunction()	 
+
+'''varun code problem 3.......................
+
+def largest_prime():
+        num = 600851475143L
+        new_num = num
+        largest_prime = 0
+        counter = 2
+        while(counter * counter <= new_num):
+                if(new_num%counter ==0):
+                        new_num = new_num / counter
+                        largest_prime = counter
+                else:
+                        counter += 1
+        if new_num > largest_prime:
+                largest_prime = new_num
+        print largest_prime
+
+if __name__ == '__main__':
+        largest_prime()'''
